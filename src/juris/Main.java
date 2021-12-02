0300b337e9b0f7e832651d3bf26492d0d51afcdd
jabolins <juris.abolins.cs@gmail.com>
2021-12-02 13:08:27 +0200

    Add first branch

--- src/juris/Main.java
+++ src/juris/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("It is base of project");
+        System.out.println("And It is my first branch");
     }
 }
